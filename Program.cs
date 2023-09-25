545cc2938acb6601cae1cd4c89462c13dec0f547
junggtail <e107ti@etec.sp.gov.br>
2023-09-25 20:10:35 -0300

    atividade troca de variáveis

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿﻿string nome1, nome2, auxiliar;
+
+Console.WriteLine("--- Troca de Valores ---\n");
+
+Console.Write("Fernanda..: ");
+nome1 = Console.ReadLine()!;
+
+Console.Write("Laura..: ");
+nome2 = Console.ReadLine()!;
+
+auxiliar = nome1;
+nome1 = nome2;
+nome2 = auxiliar;
+
+Console.WriteLine("\nApós trocar os valores temos:\n");
+
+Console.WriteLine($"Fernanda = {nome1}");
+Console.WriteLine($"Laura = {nome2}");
